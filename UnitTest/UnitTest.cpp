683226dd041abc0f5f27c8b0f08659dc86c421b8
OlegLaptop <oleg.rubenchik25@gmail.com>
2024-05-23 17:12:10 +0300

    Made unit tests

--- UnitTest/UnitTest.cpp
+++ UnitTest/UnitTest.cpp
@@ -12,6 +12,28 @@ namespace UnitTest
 		
 		TEST_METHOD(TestMethod1)
 		{
+			//Arrange
+			double arr[SIZE] = { 1.3, 2.2, -3.5, 0.0, 10.2 };
+			double res,res2;
+
+			//Act
+			res = sumOfRange(arr, 0, 2, SIZE);
+			res2 = sumOfRange(arr, 0, SIZE, SIZE);
+			//Assert
+			Assert::AreEqual(3.5, res);
+			Assert::AreEqual(10.2, res2);
+		}
+		TEST_METHOD(TestMethod2) {
+			//Arrange
+			double arr[SIZE] = { 1.3, 2.2, -3.5, 0.0, 10.2 };
+			double res, res2;
+
+			//Act
+			res = productOfRange(arr, 0, SIZE, SIZE);
+			res2 = productOfRange(arr, 1, 3, SIZE);
+			//Assert
+			Assert::AreEqual(-7.7, res2);
+			Assert::AreEqual(0.0, res);
 		}
 	};
 }
