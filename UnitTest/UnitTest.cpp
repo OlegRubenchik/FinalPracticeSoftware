cec8ea3e442c9ed826e3961c3a7e011a1e6557ce
OlegLaptop <oleg.rubenchik25@gmail.com>
2024-05-23 16:43:11 +0300

    Added test project

---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../FinalPractice/FinalPractSol.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
